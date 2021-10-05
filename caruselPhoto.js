91d9ff1d995e6f921ba2a92d11ffca83aebd1af2
allen-99 <kn99.allen@gmail.com>
2021-10-05 14:51:03 +1000

    end of the changes

--- caruselPhoto.js
+++ caruselPhoto.js
@@ -1,12 +1,15 @@
+
 $('section.awSlider .carousel').carousel({
     pause: "hover",
-    interval: 2000
-});
+    interval: 5000 });
+  
+  
 
-var startImage = $('section.awSlider .item.active > img').attr('src');
-$('section.awSlider').append('<img src="' + startImage + '">');
-
-$('section.awSlider .carousel').on('slid.bs.carousel', function () {
+  
+  $('section.awSlider .carousel').on('slid.bs.carousel', function () {
     var bscn = $(this).find('.item.active > img').attr('src');
-    $('section.awSlider > img').attr('src',bscn);
-});
+    $('section.awSlider > img').attr('src', bscn);
+  });
+  
+  
+  
